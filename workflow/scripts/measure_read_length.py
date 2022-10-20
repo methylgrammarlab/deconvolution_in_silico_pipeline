0216bf4d5b0922ec224eca8cceede9364e224272
Ben Berman <benbfly@gmail.com>
2022-10-20 14:56:24 +0300

    added license headers

--- workflow/scripts/measure_read_length.py
+++ workflow/scripts/measure_read_length.py
@@ -2,6 +2,32 @@
 This script takes in an epiread file and counts
 how many CpGs each reads covers, then returns the mean.
 '''
+
+# ###################################################
+#
+# MIT License
+#
+# Copyright (c) 2022 irene unterman and ben berman
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# ###################################################
+
 import json
 from epiread_tools.naming_conventions import *
 METHYLATION_COLUMN = 7
